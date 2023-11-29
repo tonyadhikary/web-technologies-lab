f1cc138cf08799dee8a50b3af1bdc56dbb23bec7
Jayanta Adhikary <jayantaadhikary@protonmail.com>
2023-11-29 15:01:39 +0530

    lab 11

--- lab11/index.js
+++ lab11/index.js
@@ -1,13 +1,25 @@
 const button = document.querySelector("button");
 const p = document.querySelector("p");
 
-async function fetchData() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users/2");
-  const data = await response.json();
+let counter = 1;
 
-  console.log(data);
+function fetchData() {
+  // const response = await fetch("https://jsonplaceholder.typicode.com/users/2");
+  // const data = await response.json();
 
-  p.innerHTML = data.name;
+  fetch(`https://jsonplaceholder.typicode.com/users/${counter}`).then(
+    (response) => {
+      response.json().then((data) => {
+        p.innerHTML = data.name;
+      });
+    }
+  );
+  // p.innerHTML = data.name;
+  counter++;
+
+  if (counter > 10) {
+    counter = 1;
+  }
 }
 
 button.addEventListener("click", () => fetchData());
