f2942505476e69112431ecbd7c7eaeefacb7640b
Jayanta Adhikary <jayantaadhikary@protonmail.com>
2023-11-29 23:19:47 +0530

    labs update

--- lab11&12/index.js
+++ lab11&12/index.js
@@ -6,17 +6,14 @@ let counter = 1;
 function fetchData() {
   // const response = await fetch("https://jsonplaceholder.typicode.com/users/2");
   // const data = await response.json();
-
-  fetch(`https://jsonplaceholder.typicode.com/users/${counter}`).then(
-    (response) => {
-      response.json().then((data) => {
-        p.innerHTML = data.name;
-      });
-    }
-  );
+  fetch("data.json").then((response) => {
+    response.json().then((data) => {
+      data = data[counter];
+      p.innerHTML = `<h3>${data.name}</h3> <p>${data.email}</p> <p>${data.phone}</p> <p>${data.website}</p>`;
+    });
+  });
   // p.innerHTML = data.name;
   counter++;
-
   if (counter > 10) {
     counter = 1;
   }
